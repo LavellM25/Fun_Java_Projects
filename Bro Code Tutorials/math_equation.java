f35660f41e0c174479f4460fb71159748b6e6a40
LavellM25 <Lavellmcgrone25@gmail.com>
2025-01-04 22:46:06 -0600

    math_equation.java

---
+++ Bro Code Tutorials/math_equation.java
@@ -0,0 +1,13 @@
+/* Date: 12-20-2024
+   Description: Learning how to write basic arithmetic equations in Java.
+ */
+
+public class MathEquation {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int result = (a * b) + (a - b);
+
+        System.out.println("The result of (a * b) + (a - b) is: " + result);
+    }
+}
