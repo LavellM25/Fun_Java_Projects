bc441028a439bc3ba884353a0fe76634093c59d4
LavellM25 <Lavellmcgrone25@gmail.com>
2025-01-04 22:49:10 -0600

    helloworld.java

---
+++ Bro Code Tutorials/helloworld.java
@@ -0,0 +1,10 @@
+/* Date: 12-1-2024
+   Description: Working with Java for the first time, learning how to print Hello World to console.
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
