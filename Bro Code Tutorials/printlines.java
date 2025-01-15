808c019d5fe31a9ee1c0f11788cb8b58110776eb
LavellM25 <Lavellmcgrone25@gmail.com>
2025-01-14 22:04:02 -0600

    printlines.java

---
+++ Bro Code Tutorials/printlines.java
@@ -0,0 +1,11 @@
+// 1-1-2025
+
+public class printlines {
+    public static void main(String[] args) {
+        // Print a line of equal signs
+        System.out.println("============================");
+
+        // Print a line of dashes
+        System.out.println("-----------------------");
+    }
+}
