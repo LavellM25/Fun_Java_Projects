d8dc87b39fe5cd3b176326c8a13526eb31b5c6ec
LavellM25 <Lavellmcgrone25@gmail.com>
2025-01-20 20:29:37 -0600

    HelloWorld.java

---
+++ Bro Code Tutorials/Loops/HelloWorld.java
@@ -0,0 +1,15 @@
+/* Date: 1-20-2025
+   Description: Program that prints "hello" 10 times using a while loop. 
+/
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        int count = 0; // Initialize a counter variable
+        
+        // While loop that runs until count is less than 10
+        while (count < 10) {
+            System.out.println("hello"); // Print "hello"
+            count++; // Increment the counter
+        }
+    }
+}
