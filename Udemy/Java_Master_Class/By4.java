8d2091b18b8b37faab3d3f53f21e1e40b118b399
LavellM25 <Lavellmcgrone25@gmail.com>
2025-01-21 20:51:08 -0600

    By4.java

---
+++ Udemy/Java_Master_Class/By4.java
@@ -0,0 +1,12 @@
+public class DivisibleByFour {
+    public static void main(String[] args) {
+        int number = 20; // Test number
+
+        // Check if the number is divisible by 4
+        if (number % 4 == 0) {
+            System.out.println(number + " is divisible by 4");
+        } else {
+            System.out.println(number + " is not divisible by 4");
+        }
+    }
+}
