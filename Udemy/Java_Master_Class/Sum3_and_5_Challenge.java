a0fad5bf8b74f7b04a1bc45915e94978e3e6eb9e
LavellM25 <Lavellmcgrone25@gmail.com>
2025-01-26 15:15:42 -0600

    Create Sum3_and_5_Challenge.java
    For statement challenge

---
+++ Udemy/Java_Master_Class/Sum3_and_5_Challenge.java
@@ -0,0 +1,29 @@
+/* Date: 1-26-2025
+   Description: Loop through #'s 1 to 1000 to get the sum of all numbers divisible by 3 and 5.
+                Learn how for loop statements works.
+
+ */
+
+public class Sum3_and_5_Challenge {
+    public static void main(String[] args) {
+
+        // Loop through #'s 1 to 1000 to get the sum of all numbers divisible by 3 and 5.
+        // print all #'s met the conditions.
+        // break out of the loop if you have found 5 #'s that met the conditions.
+        int countofMatches = 0;
+        int sumOfMatches = 0;
+
+        for (int loopNumber = 1; loopNumber <= 1000; loopNumber++){
+            if ((loopNumber % 3 == 0) && (loopNumber % 5== 0)){
+              countofMatches++;
+              sumOfMatches += loopNumber;
+              System.out.println("Found a match = " + loopNumber);
+            }
+            if (countofMatches == 5){
+                break;
+            }
+        }
+
+        System.out.println("Sum= " + sumOfMatches);
+    }
+}
