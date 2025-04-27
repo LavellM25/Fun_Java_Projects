75e2627847898a6dff8be567fb597973b0de0c10
LavellM25 <Lavellmcgrone25@gmail.com>
2025-04-26 22:34:24 -0500

    exercise 43

--- Udemy/Java_Master_Class_2025/Section_9_Advanced_Arrays/Coding_Exercises/Exercise43/README.md
+++ Udemy/Java_Master_Class_2025/Section_9_Advanced_Arrays/Coding_Exercises/Exercise43/README.md
@@ -19,6 +19,26 @@ You'll write a method that **reverses an array without creating a new one**, hel
 
 ---
 
+## 🧠 Pseudocode for reverse(int[] array)
+1. Print the array (original order):
+    - Print "Array = " followed by the array elements
+
+2. Set two pointers:
+    - One pointer (start) at the beginning of the array (index 0)
+    - One pointer (end) at the end of the array (last index)
+
+3. While start is less than end:
+   - a. Save the value at start index into a temporary variable
+   - b. Replace the value at start index with the value at end index
+   - c. Replace the value at end index with the value saved in temporary variable
+   - d. Move start pointer one step forward
+   - e. Move end pointer one step backward
+
+4. Print the array again (after reversing):
+    - Print "Reversed array = " followed by the reversed array elements
+
+---
+
 ## 🔎 Example
 **Input:**
 ```java
--- Udemy/Java_Master_Class_2025/Section_9_Advanced_Arrays/Coding_Exercises/Exercise43/ReverseArray.java
+++ Udemy/Java_Master_Class_2025/Section_9_Advanced_Arrays/Coding_Exercises/Exercise43/ReverseArray.java
@@ -1,41 +1,22 @@
 package Section_9_Advanced_Arrays.Coding_Exercises.Exercise43;
 
-/*
-
-Coding Exercise 43:
-Reverse an Array: Mastering In-Place Swaps for Efficient Reordering
-Write a method called reverse() with an int array as a parameter.
-
-The method should not return any value. In other words, the method is allowed to modify the array parameter.
-
-
-
-To reverse the array, you have to swap the elements, so that the first element is swapped with the last element and so on.
-
-For example, if the array is [1, 2, 3, 4, 5], then the reversed array is [5, 4, 3, 2, 1].
-
-
-
-The method should first print out the newly passed in array as Array = [1, 2, 3, 4, 5]
-
-and then once it's been reversed, print it out as Reversed array = [5, 4, 3, 2, 1]
-
-
-
-TIP: When swapping the elements, use a variable to temporarily hold the current element.
-
-
-
-NOTE: The method should be defined as private static.
-
-NOTE: Do not add a main method to the solution code.
-
-NOTE: Classes that are not in the java.lang package should be manually imported.
-
-Learning Objective: Practice reversing an array by swapping elements to deepen your understanding of indexing, loop boundaries, and in-place transformations for data ordering.
-
-
-
+/** Pseudocode:
+ * 1. Print the array (original order):
+ *     - Print "Array = " followed by the array elements
+ *
+ * 2. Set two pointers:
+ *     - One pointer (start) at the beginning of the array (index 0)
+ *     - One pointer (end) at the end of the array (last index)
+ *
+ * 3. While start is less than end:
+ *     a. Save the value at start index into a temporary variable
+ *     b. Replace the value at start index with the value at end index
+ *     c. Replace the value at end index with the value saved in temporary variable
+ *     d. Move start pointer one step forward
+ *     e. Move end pointer one step backward
+ *
+ * 4. Print the array again (after reversing):
+ *     - Print "Reversed array = " followed by the reversed array elements
  */
 public class ReverseArray {
 }
